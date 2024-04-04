1fff76a73e4a4c2e9ebed96d46071229946f7e67
phantomesquid0928 <phantomsquid0928@gmail.co>
2024-04-04 23:03:32 +0900

    add: added variable that indicates if staged. (istrack)      adding stage files on mockfs to have newfiles on mockfs

--- #p2_20192419/status.c
+++ #p2_20192419/status.c
@@ -1,6 +1,12 @@
 
 #include "phantomutils.h"
 
+queue q;
+queue untracked;
+queue changes;
+queue newall;
+
+
 int main(int argc, char * argv[]) {
     if (argc != 1) {
         printf("ERROR no args allowed\n");
@@ -35,10 +41,97 @@ int main(int argc, char * argv[]) {
     // show_staging_log();
     show_fs(version_cursor->root, "");
     stagelog * temp = head;
-    while(temp) {
-        temp->log;
+
+    while(temp) { //o logn logn
+        char * oripath = temp->log;
+        char * name = substr(oripath, return_last_name(oripath) + 1, strlen(oripath));
+        filedir * f = newfile();
+        strcpy(f->oripath, oripath);
+        strcpy(f->name, name);
+        //as this is temp file, there is no path (commitpath)
+        f->istrack = 1;
+        filedir * found = addfiledir(f, 1);
+        // if (found == NULL) { //staged new file, not 
+        //     temp->isnew = 1;
+        // }
+        
         temp = temp->next;
     }
+    show_fs(version_cursor->root, "");
+
+    struct stat statbuf;
+    struct dirent ** namelist;
+    int cnt;
+
+
+    filedir * root = version_cursor->root;
+    
+    // q = *initQueue();
+    // changes = *initQueue();
+    // untracked = *initQueue();
+    // newall = *initQueue();
+    // q.push(&q, root); //commit 한번 돌기-> commit할 파일 표시 -> 다시 돌기 -> 실제파일과 비교
+    // while(!q.empty(&q)) { // two pointer 사용 필요
+    //     filedir * f = q.front(&q);
+    //     q.pop(&q);
+    //     if ((cnt = scandir(f->oripath, &namelist, NULL, alphasort)) < 0) exit(1);
+    //     //
+    //     int i = 0, j = 0;
+    //     while(1) {
+    //         if (i > f->childscnt) { //removed
+    //             if (j > cnt) break;
+    //             if (f->childs[i]->childscnt != -1) {
+    //                 q.push(&q, f->childs[i]);
+    //             }
+    //             j++;
+    //         }
+    //         if (j == cnt) { // new
+    //             if (i > f->childscnt) break;
+    //         }
+    //         int res = strcmp(f->childs[i]->name, namelist[j]->d_name);
+    //         if (res == 0) {
+    //             if (f->childs[i]->childscnt != -1) { //dir
+    //                 q.push(&q, f->childs[i]);
+    //                 i++;
+    //                 j++;
+    //                 continue;
+    //             }
+    //             if (access(f->childs[i]->oripath, F_OK)) { //removed
+    //                 f->childs[i]->chk = 2; //rem
+    //                 changes.push(&changes, f->childs[i]);
+    //                 i++;
+    //                 j++;
+    //                 continue;
+    //             }
+    //             if (lstat(f->childs[i]->oripath, &statbuf) < 0) exit(0);
+    //             if (statbuf.st_mtime != f->childs[i]->top->statbuf.st_mtime) {//modified
+    //                 f->childs[i]->chk = 1; //mod
+    //                 changes.push(&changes, f->childs[i]);
+    //                 i++;
+    //                 j++;
+    //                 continue;
+    //             }
+    //         }
+    //         if (res < 0) { //removed
+                
+    //         }
+    //         if (res > 0) { //new
+                
+    //         }
+    //     }
+    //     for (int i = 0; i <= f->childscnt; i++) {
+    //         filedir * c = f->childs[i];
+    //         if (c->childscnt == -1) //file
+    //         {
+    //          //   if 
+                
+    //         }
+    //         else {
+    //             q.push(&q, c);
+    //         }
+    //     }
+    // }
+    
     
     /**
      * TODO: scandir cwd -> make list of files -> loop list -> find from loglist -> if not exists then 'untracked'
@@ -47,6 +140,7 @@ int main(int argc, char * argv[]) {
      * 
     */
     
+
     // file * temp = version_cursor->head;
     // loglist * temp2 = head;
     // while(temp) {
